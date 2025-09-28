ae59b4a4ac5c046df4696775ea94dcfef62255d7
daniel69zz <luisdanielrojascaceres@gmail.com>
2025-09-28 00:44:33 -0400

    badge add or new

--- src/components/comun/Badge.jsx
+++ src/components/comun/Badge.jsx
@@ -1,9 +1,16 @@
+// import { Slot } from "@radix-ui/react-slot"; // opcional, si quieres asChild
 import styles from "./Badge.module.css";
+import cn from "../utils/cn";
 
-export default function Badge({text}){
-    return (
-        <span className={styles.sp_tag} >
-            {text}
-        </span>
-    )
+
+export default function Badge({ text, className, variant = "default", asChild = false, ...props }) {
+//   const Comp = asChild ? Slot : "span";
+  return (
+    <span
+      className={cn(styles.badge, className)}
+      data-variant={variant}
+      {...props}
+    >{text}
+    </span>
+  );
 }
--- src/components/comun/Button.jsx
+++ src/components/comun/Button.jsx
@@ -1,8 +1,17 @@
 import styles from "./Button.module.css";
+import cn from "../utils/cn";
 
-export default function Button({text, onClick}){
+export default function Button({className, variant = "default", 
+    size = "md", asChild = false, text, onClick, ...props}){
     return(
-        <button className={styles.btn} onClick={onClick}>{text}</button>
+        <button 
+            className={cn(styles.btn, className)}
+            data-variant={variant}
+            data-size={size} 
+            onClick={onClick}
+            {...props}
+        >{text}
+        </button>
     );
 }
 
