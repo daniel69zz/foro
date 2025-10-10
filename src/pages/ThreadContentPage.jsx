5ae6d9d69c7e9912abb88e1d16aaea5603ed2ce9
daniel69zz <luisdanielrojascaceres@gmail.com>
2025-10-09 21:23:38 -0400

    mejoras

--- src/pages/ThreadContentPage.jsx
+++ src/pages/ThreadContentPage.jsx
@@ -3,11 +3,25 @@ import { ThreadCard } from "../components/threads/ThreadCard";
 import { comments_post, thread_id} from "../components/utils/data_static";
 
 import { useParams } from "react-router-dom";
+import { useState } from "react";
+
+function add_commment(authorId, content){
+    const newComment = {
+      id: `${hilo.id}c${comments.length + 1}`,
+      authorId,
+      createdAt: new Date().toISOString(),
+      content
+    };
+
+    setComments([...comments, newComment]);
+}
 
 export function ThreadContentPage(){
     const { id } = useParams();
     const comments = comments_post(id);
     const hilo = thread_id(id);
+
+
     return (
         <main>
             <div className="ThreadHeader">
